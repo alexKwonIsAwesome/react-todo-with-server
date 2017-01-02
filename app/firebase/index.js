e5c32c69c017b6f1885c7e42972a99252444b64a
alexKwonIsAwesome <khwalex@gmail.com>
2017-01-02 18:58:38 +0100

    update: moved firebased credentials to env

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyBDqadYGEG847tUqxFODIlfacVd48Q6YMc",
-    authDomain: "alex-todo-app-bbd97.firebaseapp.com",
-    databaseURL: "https://alex-todo-app-bbd97.firebaseio.com",
-    storageBucket: "alex-todo-app-bbd97.appspot.com",
-    messagingSenderId: "490787991651"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   
   firebase.initializeApp(config);
